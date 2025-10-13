c6de1cb9696a9d46f244f1090a2c272f8d15ba94
ehroy <anjaykoko69@gmail.com>
2025-10-13 11:00:48 +0700

    template laravel + vue

---
+++ resources/js/Pages/justorange-default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+<!-- Page Container -->
+<div class="flex items-center justify-center min-h-screen bg-white py-48">
+    <div class="flex flex-col">
+     
+
+        <!-- Error Container -->
+        <div class="flex flex-col items-center">
+            <div class="text-amber-500 font-bold text-7xl">
+                JeeBlog
+            </div>
+
+            <div class="font-bold text-3xl xl:text-4xl lg:text-6xl md:text-5xl mt-10">
+                Buat blog gratis dengan mudah.
+            </div>
+
+            <div class="text-gray-400 font-medium text-sm md:text-xl lg:text-2xl mt-8">
+                Daftar > Pilih template > Download > Upload ke hosting anda!
+            </div>
+        </div>
+
+    
+    </div>
+</div>
+    </div>
+</template>
